33d561ed593ff2478dc146702171d20cc1c8bf9b
Animesh <kanimesh.s@gmail.com>
2020-02-03 16:37:46 +0530

    basic hooks unit tests

---
+++ __tests__/DataTableHooks-test.js
@@ -0,0 +1,33 @@
+import { act } from 'react-dom/test-utils';
+import useRows from '../src/utils/useRows';
+import { testHook } from '../config/testHook';
+
+let filterTestRows;
+let paginationTestRows;
+beforeEach(() => {
+  testHook(() => {
+    filterTestRows = useRows({
+      filters: [{id: "someCol", value: "a"}],
+      page: 0,
+      pageSize: 10,
+      pagination: {},
+      rows: [{someCol: "a"}, {someCol: "b"}]
+    });
+    paginationTestRows = useRows({
+      filters: [],
+      page: 1,
+      pageSize: 10,
+      pagination: {},
+      rows: [{someCol: "a"}, {someCol: "b"}]
+    });
+  });
+});
+
+describe('useRows', () => {
+  test('filterRow fn', () => {
+    expect(filterTestRows[0].length).toBe(1);
+  });
+  test('testing pagination', () => {
+    expect(paginationTestRows[0].length).toBe(0);
+  });
+});
